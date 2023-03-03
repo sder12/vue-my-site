c6b0f453203d486d7a22fbe385735e6e78c16370
sder12 <sara.cetto@gmail.com>
2023-03-03 12:23:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+
+<template>
+  <!-- header -->
+  <!-- <AppHeader /> -->
+  <header>
+    questo è un header
+  </header>
+  <!-- /header -->
+
+  <!-- main -->
+  <router-view></router-view>
+  <!-- /main -->
+
+  <!-- footer -->
+  <!-- <AppFooter /> -->
+  <!-- /footer -->
+</template>
+
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+
+export const store = reactive({
+    navLink: [
+        {
+            name: 'Home'
+        }
+    ],
+});
